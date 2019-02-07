a86c895e26b76c0ff92732389a474de54366416e
SovietSpy2 <sovietspy2@yandex.com>
2019-02-07 22:37:20 +0100

    added session management

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const withAuth = require('./middleware');
 const mongodb = require('mongodb');
 require('dotenv').config();
 const User = require("./entities/user");
+const CONSTANTS = require("./constants");
 
 const secret = 'mysecretsshhh';
 
@@ -26,7 +27,7 @@ app.use(bodyParser.json()); // support json encoded bodies
 
 
 app.use(morgan('dev'));
-app.use(errorHandler());
+
 
 
 
@@ -44,7 +45,7 @@ mongodb.MongoClient.connect(url, {useNewUrlParser: true}, (error, client) => {
 
     app.get('/home', routes.home);
 
-    app.get('/api/secret', function(req, res) {
+    app.get('/api/secret',withAuth, function(req, res) {
         res.send('The password is potato');
     });
 
@@ -64,7 +65,42 @@ mongodb.MongoClient.connect(url, {useNewUrlParser: true}, (error, client) => {
       });
 
 
-    
+      app.post('/api/authenticate', function(req, res, next) {
+        const { username, password } = req.body;
+        console.log(username);
+        req.db.collection(CONSTANTS.USERS).findOne({ username: username }, function(err, user) {
+              if (err) console.log("elbaszodott");
+              if (user) {
+                console.log(user);
+                  // Issue token
+
+                  if (user.password === password) {
+
+                  const payload = { username };
+                  const token = jwt.sign(payload, secret, {
+                    expiresIn: '1h'
+                  });
+                  //res.status(200).send();
+                  res.cookie('token', token, { httpOnly: true }).sendStatus(200);
+                } else {
+                  res.status(403).send();
+                }
+            }
+            
+          });
+    });
+
+    app.post('/logout', withAuth, function(req,res) {
+        console.log("LOGGING OUT");
+        res.clearCookie("token").send();
+    });
+
+    app.get('/checkToken', withAuth, function(req, res) {
+      console.log(req.body);
+      res.sendStatus(200);
+    });
+
+    app.use(errorHandler());
     app.listen(port);
 
 });
